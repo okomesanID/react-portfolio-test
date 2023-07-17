2ea965ca10d15d8c07342b655b3ed4a076e3fac4
okomesan <103940417+okomesanID@users.noreply.github.com>
2023-07-17 23:34:31 +0900

    雛形完成

---
+++ src/components/Blog.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Minus consequuntur hic nesciunt architecto
+        autem ex illum facilis repudiandae optio ducimus, dolorum, ut harum
+        blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Minus consequuntur hic nesciunt
+        architecto autem ex illum facilis repudiandae optio ducimus, dolorum, ut
+        harum blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Minus consequuntur hic
+        nesciunt architecto autem ex illum facilis repudiandae optio ducimus,
+        dolorum, ut harum blanditiis esse magni eos, inventore temporibus velit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Minus consequuntur hic nesciunt architecto
+        autem ex illum facilis repudiandae optio ducimus, dolorum, ut harum
+        blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Minus consequuntur hic nesciunt
+        architecto autem ex illum facilis repudiandae optio ducimus, dolorum, ut
+        harum blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Minus consequuntur hic
+        nesciunt architecto autem ex illum facilis repudiandae optio ducimus,
+        dolorum, ut harum blanditiis esse magni eos, inventore temporibus velit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Minus consequuntur hic nesciunt architecto
+        autem ex illum facilis repudiandae optio ducimus, dolorum, ut harum
+        blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Minus consequuntur hic nesciunt
+        architecto autem ex illum facilis repudiandae optio ducimus, dolorum, ut
+        harum blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Minus consequuntur hic
+        nesciunt architecto autem ex illum facilis repudiandae optio ducimus,
+        dolorum, ut harum blanditiis esse magni eos, inventore temporibus velit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Minus consequuntur hic nesciunt architecto
+        autem ex illum facilis repudiandae optio ducimus, dolorum, ut harum
+        blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Minus consequuntur hic nesciunt
+        architecto autem ex illum facilis repudiandae optio ducimus, dolorum, ut
+        harum blanditiis esse magni eos, inventore temporibus velit. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Minus consequuntur hic
+        nesciunt architecto autem ex illum facilis repudiandae optio ducimus,
+        dolorum, ut harum blanditiis esse magni eos, inventore temporibus velit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        consequuntur hic nesciunt architecto autem ex illum facilis repudiandae
+        optio ducimus, dolorum, ut harum blanditiis esse magni eos, inventore
+        temporibus velit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Minus consequuntur hic nesciunt architecto autem ex illum facilis
+        repudiandae optio ducimus, dolorum, ut harum blanditiis esse magni eos,
+        inventore temporibus velit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Minus consequuntur hic nesciunt architecto autem ex
+        illum facilis repudiandae optio ducimus, dolorum, ut harum blanditiis
+        esse magni eos, inventore temporibus velit.
+      </p>
+    </div>
+  );
+};
+
+export default Blog;
